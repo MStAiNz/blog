1449e020c48ed277a122cc52ee7898f05b02439f
MStAiNz <igwewashere@gmail.com>
2025-12-03 10:15:53 -0800

    Complete Django Blog

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
-from .models import Category, Blog
+from .models import Category, Blog, Comment
 
 class BlogAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
-    list_display = ('title', 'author', 'status', 'is_featured', 'created_at', 'category')
-    search_fields = ('title', 'id ', 'category__category_name', 'status')
+    list_display = ('title', 'category', 'author', 'status', 'is_featured')
+    search_fields = ('id', 'title', 'category__category_name', 'status')
     list_editable = ('is_featured',)
 
-# Register your models here.
 admin.site.register(Category)
 admin.site.register(Blog, BlogAdmin)
+admin.site.register(Comment)
